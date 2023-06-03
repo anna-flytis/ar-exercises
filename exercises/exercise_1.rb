71aaa2d59c003faba1989847cc69039ac10aa54c
Anna Mariakhina <77463611+anna-flytis@users.noreply.github.com>
2023-06-03 10:31:10 -0600

    Update exercise_1.rb

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -6,3 +6,8 @@ puts "----------"
 Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts 'Stores created successfully'
+
+store_count = Store.count
+puts "Number of stores: #{store_count}"
