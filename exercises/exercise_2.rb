decec670a343a3efa1f534da53f86ce581577ab8
Anna Mariakhina <77463611+anna-flytis@users.noreply.github.com>
2023-06-03 10:32:46 -0600

    Update exercise_2.rb

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,7 +4,12 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-puts 'Stores created successfully'
+# Load the first store (id = 1) from the database and assign it to @store1
+@store1 = Store.find(1)
 
-store_count = Store.count
-puts "Number of stores: #{store_count}"
+# Load the second store from the database and assign it to @store2
+@store2 = Store.find(2)
+
+# Update the first store (@store1) instance in the database
+@store1.update(name: 'New Store Name')
+
