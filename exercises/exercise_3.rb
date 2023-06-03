99e773c313a32d7ca8a37ecfefd3a167c6234dd2
anna-flytis <a.mariakhina@gmail.com>
2023-06-03 10:38:53 -0600

    final

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -5,4 +5,10 @@ require_relative './exercise_2'
 puts "Exercise 3"
 puts "----------"
 
-# Your code goes here ...
+@store3 = Store.find(3)
+
+@store3.destroy
+
+store_count = Store.count
+puts "Number of stores after deletion: #{store_count}"
+
